67ebac2d98f16794863dc6281f9c3dc19f5a1908
whitedevil2014 <75196911+whitedevil2014@users.noreply.github.com>
2020-11-30 11:46:42 +0530

    Add files via upload

---
+++ RootMe.sh
@@ -0,0 +1,9 @@
+apt install figle
+figlet -f big Cyber Ninza Hacker
+apt update && apt upgrade -y
+git clone https://github.com/TamilHackz/RootMe.git
+cd RootMe
+ls
+chmod 777 rootme.sh
+bash rootme.sh
+rootme
